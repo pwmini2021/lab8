d30a9ca00c26717dd18157cdf2103120d10a4b13
mkle <mkle@netcompany.com>
2021-11-30 00:52:36 +0100

    lab8 init

---
+++ src/components/employeeList/EmployeeListItem.test.tsx
@@ -0,0 +1,15 @@
+// noinspection DuplicatedCode
+
+import React from "react";
+
+/*
+getBy - finds or throws error - for standard use
+queryBy - finds or returns null - used to verify that something is not there
+findBy - async, finds or throws
+*/
+
+describe('EmployeeListItem test test', () => {
+    test('EmployeeListItem dummy test', async () => {
+        expect(true).toBeFalsy(); //Not implemented yet
+    });
+});
